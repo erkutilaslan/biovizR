97a8ce26c05985f30de4b8a4be7b8c5d3fa447d6
erkutilaslan <erkutilaslan@gmail.com>
2020-09-03 22:56:58 +0200

    add maplot_MS-IP function to biovizR

--- R/maplot_IP-MS.R
+++ R/maplot_IP-MS.R
@@ -1,20 +1,18 @@
-#this script is to visualize MS-IP results with 2 axis
-#for number of validated peptides in NANOS-IP vs pCMV6-IP
-#when i run the script manually it generates proper plot
-#but when i run it as a function, it proceeds without error
-#and creates pdf file but the file doesnt open.
+#' Dot plot for IP-MS experiment visualization.
+#'
+#' This function visualizes IP-MS results with 2 axis
+#' for number of validated peptides in NANOS-IP vs pCMV6-IP
+#' when i run the script manually it generates proper plot
+#' but when i run it as a function, it proceeds without error
+#' and creates pdf file but the file doesnt open.
+#'
+#' @param ip_data Path to the input file
+#' @return A dot plot of the  ip_data
+#' @export
 
-library(tidyverse)
-library(readxl)
-
-plot_ipms(x)
-
-x <- "~/nanos_test.xlsx"
-
-#importing MS-IP data
-###########################################
 plot_ipms <- function(ip_data) {
 
+#data import
 ip_1 <- read_xlsx(ip_data,
                   sheet = 3)
 ip_2 <- read_xlsx(ip_data,
@@ -30,7 +28,6 @@ ctrl_3 <- read_xlsx(ip_data,
                   sheet = 8)
 
 #data cleanup and process
-###########################################
 
 #removing unnecesary columns
 ip_1 <- ip_1[, c(4, 8)]
@@ -72,16 +69,14 @@ ipms <- ipms %>% mutate("avg.ip" = (ip_1 + ip_2 + ip_3) / 3)
 ipms <- ipms %>% mutate("avg.ctrl" =  (ctrl_1 + ctrl_2 + ctrl_3) / 3)
 
 #generating ma-plot
-###########################################
 
-pdf("IP-MS_plot.pdf")
 ip_plot <- ggplot(ipms, aes(avg.ip, avg.ctrl), label = HGNC)
-ip_plot + geom_point() +
+final_plot <- ip_plot + geom_point() +
     geom_text(aes(label = ifelse(avg.ctrl < 2,
                                  as.character(HGNC),
                                  "")),
                   hjust = 0, vjust = 0)
-dev.off()
+plot(final_plot)
 
 }
 

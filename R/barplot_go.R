d8fbf42fc8d2b97468c343c4a71f0ace3776f7e7
erkutilaslan <erkutilaslan@gmail.com>
2020-09-22 21:58:38 +0200

    add type function to barplot

--- R/barplot_go.R
+++ R/barplot_go.R
@@ -3,16 +3,70 @@
 #' This function visualizes gene ontology results from ClueGO as bar plot.
 #'
 #' @param go_data Input GO data to visualize.
+#' @param type Default cluego. Parameter to specify input data source.
 #' @param top Turned off by default. Parameter to set top amount of processes to be visualized.
 #' @param go_process Turned off by default. Parameter to add a specific keyword to only visuzalize GO terms that contains it.
 #' @return A bar plot of gene ontology results.
 #' @import tidyverse
 #' @export
 
-barplot_go <- function(go_data, top = " ", go_process = " ") {
+barplot_go <- function(go_data, type = "cluego", top = " ", go_process = " ") {
 
 # import data
 go_data <- readxl::read_excel(go_data, sheet = 1)
+
+if (type = "cluego") {
+
+# first removing unnecessary columns they interfere with deduplication
+go_data <- dplyr::select(go_data, -6, -7, -8, -9)
+
+# deduplication
+go_data <- dplyr::distinct(go_data)
+
+# converting column name so its easier to mutate
+colnames(go_data)[5] <- "padj"
+
+# -log10 conversion of padj by mutate
+go_data <- dplyr::mutate(go_data, log10_padj = -log10(go_data$padj))
+
+# arranging the GO Terms in descanding order
+go_data <- dplyr::arrange(go_data, dplyr::desc(go_data$log10_padj))
+
+}
+
+if (type = "panther") {
+
+# first removing unnecessary columns they interfere with deduplication
+
+# deduplication
+
+# converting column name so its easier to mutate
+
+
+}
+
+if (type = "david") {
+
+# first removing unnecessary columns they interfere with deduplication
+
+# deduplication
+
+# converting column name so its easier to mutate
+
+
+}
+
+if (type = "gprofiler") {
+
+# first removing unnecessary columns they interfere with deduplication
+
+# deduplication
+
+# converting column name so its easier to mutate
+
+
+}
+
 
 # selecting GOTerms for visualization
 if (go_process != " ") {
@@ -21,33 +75,7 @@ go_data <- go_data[grep(go_process,
                        ignore.case = TRUE), ]
 }
 
-# deduplication. because of some terms are present in multiple
-# GO groups they are duplicated
-
-# first removing 'group' columns they interfere with deduplication
-
-go_data <- dplyr::select(go_data, -6, -7, -8, -9)
-
-# deduplication
-
-go_data <- dplyr::distinct(go_data)
-
-# before visualization I need to convert adj-p by -log10 for visualization
-
-# first converting column name so its easier to mutate
-
-colnames(go_data)[5] <- "padj"
-
-# -log10 conversion of padj by mutate
-
-go_data <- dplyr::mutate(go_data, log10_padj = -log10(go_data$padj))
-
-# for long GO lists I want to visualize only top 10/20.
-# this is not possible with top = 10 command
-# while generating plots. so i will generate new tables of top 10 top 20.
-
-go_data <- dplyr::arrange(go_data, dplyr::desc(go_data$log10_padj))
-
+#selecting top processes for visualization
 if (top != " ") {
 
 go_data <- dplyr::slice(go_data, 1:top)

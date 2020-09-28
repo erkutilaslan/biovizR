09fa55c5ac279223847ce85170e93ae11093c001
erkutilaslan <erkutilaslan@gmail.com>
2020-09-28 23:33:37 +0200

    add data process fix to maplot_dge. remove bubbleplot_go func

--- R/maplot_dge.R
+++ R/maplot_dge.R
@@ -5,22 +5,20 @@
 #' @param dge_data Path to the input file.
 #' @param FDR Default 0.01. Adjust FDR value.
 #' @param FC Default 0.5. Adjust log2FC threshold.
+#' @param TOP Default 10. Adjust top number of DE genes to visualize.
 #' @return An ma-plot of RNA-Seq DGE data.
 #' @export
-
-maplot_dge <- function(dge_data, FDR = 0.01, FC = 0.5) {
+maplot_dge("~/biovizR/data/maplot_data.csv")
+maplot_dge <- function(dge_data, FDR = 0.01, FC = 0.5, TOP = 10) {
 
 #data import
 dge_data <- read.table(file = dge_data,
 			  header = TRUE, sep = ",", dec = ".")
 
-#head(dat)
-
-#I need to process this once again and remove collumns etc 
-dge_data$SUID <- NULL
-dge_data$selected <- NULL
-dge_data$shared.name <- NULL
-dge_data$x <- NULL
+#processing colnames for visualization
+colnames(dge_data)[2] = "baseMean"
+colnames(dge_data)[3] = "log2FoldChange"
+colnames(dge_data)[7] = "padj"
 
 # Visuzalization
 
@@ -28,7 +26,8 @@ final_plot <- ggpubr::ggmaplot(dge_data, main = " ",
          fdr = FDR, fc = FC, size = 1.5,
          palette = c("#B31B21", "#1465AC", "darkgray"),
          genenames = as.vector(dge_data$HGNC),
-         legend = "top", top = 0,
+         legend = "top",
+	 top = TOP,
          font.label = c("bold", 14),
          label.rectangle = TRUE,
          font.legend = "bold",

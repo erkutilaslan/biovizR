c3315d95a79e99da130fd1fc756c36c7e2e63080
erkutilaslan <erkutilaslan@gmail.com>
2020-11-05 16:43:56 +0100

    add percentage, sd and pvalue calculation

--- R/barplot_qpcr.R
+++ R/barplot_qpcr.R
@@ -10,8 +10,10 @@
 #' @return A bar plot of qPCR results.
 #' @import tidyverse
 #' @export
-install.packages("tidyverse")
-install.packages("xml2")
+
+library(tidyverse)
+library(ggpubr)
+
 barplot_qpcr <- function(qpcr_data,
                          type = "biorad",
                          ref = "",
@@ -23,7 +25,6 @@ qpcr_data <- read.csv(qpcr_data)
 qpcr_data <- read.csv("~/biovizR_data/qpcr_data.csv")
 qpcr_data <- read.csv("~/qpcr_data.csv")
 
-#try not to remove "Target" and I will use it as fill parameter in the barplot
 #data wrangling
 qpcr_data <- qpcr_data[, c(4, 6, 9)]
 qpcr_data$Sample[qpcr_data$Sample == ""] <- NA
@@ -33,14 +34,14 @@ qpcr_data <- dplyr::distinct(qpcr_data)
 qpcr_data$Cq.Mean <- as.numeric(as.character(qpcr_data$Cq.Mean))
 
 #pivot_wider for analysis
-qpcr_data <- tidyr::pivot_wider(qpcr_data, names_from = Target, values_from = Cq.Mean )
+qpcr_data <- tidyr::pivot_wider(qpcr_data, names_from = Target, values_from = Cq.Mean)
 
 #remove so that we can write the analysis script properly will be removed later on
 qpcr_data <- qpcr_data[c(-2, -5, -8), ]
 
 #analysis
 #for each sample average the ct values of the 3 ref genes --> ct[ref] = mean( ct[ref1], ct[ref[2], ct[ref3] )
-# for each sample, calculate the dct as the difference dct = ct[ref] - ct[goi]
+#for each sample, calculate the dct as the difference dct = ct[ref] - ct[goi]
 
 #dCt calculation
 qpcr_data <- dplyr::mutate(qpcr_data, dCt = (NOS1 - Actin))
@@ -63,54 +64,42 @@ target_exp <- dplyr::mutate(target_exp, target_avg.exp = mean(expression))
 qpcr_data <- plyr::join_all(list(qpcr_data, control_exp, target_exp))
 qpcr_data <- dplyr::mutate(qpcr_data, avg.exp = dplyr::coalesce(qpcr_data$target_avg.exp, control_avg.exp))
 
-#statistical analysis
+#converting raw expression to percentage for better visualization
+qpcr_data <- dplyr::arrange(qpcr_data, dplyr::desc(qpcr_data$avg.exp))
+qpcr_data <- dplyr::mutate(qpcr_data, percent.exp = qpcr_data$avg.exp/qpcr_data$avg.exp[1]*100)
+
+#calculating p-value and sd
 stats <- t.test(target_exp$NOS1, control_exp$NOS1)
-stats$p.value
-ggpubr::compare_means()
-my_comparison <- list(c("- control 24", "N1-1 24"))
-
-
-#percentage expression taking control sample 100% for better visualization
-qpcr_data[3, 1] <- "N1-1 24"
-qpcr_data[4, 1] <- "- control 24"
-qpcr_data[5, 1] <- "- control 24"
-qpcr_data[6, 1] <- "N1-1 24" 
-
-#data wrangling for visualization
-qpcr_results <- tidyr::pivot_longer()
-
+target_sd <- sd(target_exp$expression)
+control_sd <- sd(control_exp$expression)
+final_sd <- c(target_sd, control_sd)
 
 #visualization
 final_plot <- ggpubr::ggbarplot(qpcr_data,
                                 x = "Sample",
-                                y = "avg.exp",
-                                add = "mean.se",
+                                y = "percent.exp",
                                 fill = "Sample",
                                 xlab = "Sample",
                                 ylab = "Relative mRNA level",
-                                size = 0.5,
-                                palette = "npg",
-                                lab.size = 5,
-                                lab.vjust = 0.5,
-                                lab.hjust = 1.2,
-                                sort.by.groups = FALSE,
-                                ggtheme = ggpubr::theme_pubr(base_size = 14)) +
-                                ggsignif::geom_signif(comparisons = my_comparison,
-			        test = "t.test")
-
-plot(final_plot)
-
-geom_signif()
-
-data("ToothGrowth")
-
-ggbarplot(ToothGrowth, x = "dose", y = "len", add = "mean_se",
-          color = "supp", palette = "jco", 
-          position = position_dodge(0.8))+
-  stat_compare_means(aes(group = supp), label = "p.signif", label.y = 29)
+#                                size = 0.5,
+#                                palette = "npg",
+#                                lab.size = 5,
+#                                lab.vjust = 0.5,
+#                                lab.hjust = 1.2,
+#                                sort.by.groups = FALSE,
+                                ggtheme = ggpubr::theme_pubr(base_size = 14)) + 
+              ggplot2::geom_errorbar(aes(x = "N1-1 24",
+					 ymin = percent.exp[1] - target_sd,
+					 ymax = percent.exp[1] + target_sd,
+					 width = 0.25)) +
+              ggplot2::geom_errorbar(aes(x = "- control 24",
+					 ymin = percent.exp[1] - control_sd,
+					 ymax = percent.exp[1] + control_sd,
+					 width = 0.25)) +
+              ggpubr::stat_pvalue_manual()
 
 plot(final_plot)
 return(final_plot)
 
-head(ToothGrowth)
 }
+

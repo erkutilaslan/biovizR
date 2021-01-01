9873a0d7effa793f6647b246e4c7071966382f42
erkutilaslan <erkutilaslan@gmail.com>
2021-01-01 17:04:24 +0100

    add type parameter

--- R/maplot_dge.R
+++ R/maplot_dge.R
@@ -6,23 +6,26 @@
 #' @param FDR Default 0.01. Adjust FDR value.
 #' @param FC Default 0.5. Adjust log2FC threshold.
 #' @param TOP Default 10. Adjust top number of DE genes to visualize.
+#' @param type Default deseq2. Specify input datatype.
 #' @return An ma-plot of RNA-Seq DGE data.
 #' @export
 
 
-maplot_dge <- function(dge_data, FDR = 0.01, FC = 0.5, TOP = 10) {
+maplot_dge <- function(dge_data, FDR = 0.01, FC = 0.5, TOP = 10, type = "deseq2") {
 
 #data import
 dge_data <- read.table(file = dge_data,
 			  header = TRUE, sep = ",", dec = ".")
+if (type == "deseq2") {
 
-#processing colnames for visualization
-colnames(dge_data)[2] = "baseMean"
-colnames(dge_data)[3] = "log2FoldChange"
-colnames(dge_data)[7] = "padj"
+  #processing colnames for visualization
+  colnames(dge_data)[2] = "baseMean"
+  colnames(dge_data)[3] = "log2FoldChange"
+  colnames(dge_data)[7] = "padj"
+
+}
 
 # Visuzalization
-
 final_plot <- ggpubr::ggmaplot(dge_data, main = " ",
          fdr = FDR, fc = FC, size = 1.5,
          palette = c("#B31B21", "#1465AC", "darkgray"),
